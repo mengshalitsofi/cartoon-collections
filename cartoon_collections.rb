78c9fc2c1a2837e09d04fc382f7c43f761da0d0f
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-13 20:02:13 -0800

    commit tree bfb33fa8e1eb6373abd77dc87fdf63cfe06ceb14

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do|name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do|item|
+    item.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do|item|
+    if cheese_types.include?(item)
+      return item
+    end
+  end
+  return nil
 end
